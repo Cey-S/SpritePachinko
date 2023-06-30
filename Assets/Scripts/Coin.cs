88a9a60dafd936ba30d5cb5cbb628a5cc7caff29
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2023-06-30 19:29:12 +0300

    Add Y position boundary
    Returns the coin to the object pool after it falls beyond the boundary.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private float YPosBoundary;
+
     public delegate void OnCountChange();
     public static event OnCountChange IncreaseCoinCount;
+
+    private void Update()
+    {
+        if (transform.position.y < YPosBoundary)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
